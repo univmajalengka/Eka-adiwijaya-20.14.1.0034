7a6ebde0e1e5769e11f1fa94a7701d20ae15c318
EkaAdiWijaya <73539524+EkaAdiWijaya@users.noreply.github.com>
2021-04-26 22:12:22 +0700

    Add files via upload

---
+++ tugas 2/tugas celcius.cpp
@@ -0,0 +1,18 @@
+//Nama : Eka adiwijaya
+//NPM : 201410034
+//Nama program : Tugas3.cpp
+#include<iostream>
+using namespace std;
+
+int main(){
+	float f,c;
+	
+	cout<<"Program Konversi Suhu Fahrenheit - Celsius"<<endl;
+	cout<<"=========================================="<<endl;
+	cout<<endl;
+	cout<<"Masukkan suhu dalam Fahrenheit\t: ";
+	cin>>f;
+	cout<<endl;
+	c =( f - 32)/1.8;
+	cout<<"Suhu dalam celsius adalah "<<c;
+}
